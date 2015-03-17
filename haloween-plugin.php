52c5fc27d93334ecfbd0297955be5d7d5a2c122b
Aqib Gatoo <aqib.droid@gmail.com>
2015-03-17 10:52:11 +0530

    Add Installation Hook code

--- haloween-plugin.php
+++ haloween-plugin.php
@@ -9,3 +9,15 @@ Author: Aqib Gatoo
 Author URI: http://aqibgatoo.com
 License: GPLV2
 */
+
+//call when plugin is activated
+register_activation_hook( __FILE__, 'haloween_plugin_install' );
+
+function haloween_plugin_install() {
+
+	//setup default options values
+	$hween_options_arr = array('currency_sign', '$', );
+
+	//save our default option values
+	update_option( 'haloween_options', $hween_options_arr );
+}
