3a9da5f3ecd1926acac39fe3abd2d08177753ae7
Aqib Gatoo <aqib.droid@gmail.com>
2015-03-17 10:41:12 +0530

    Initial commit

---
+++ haloween-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Haloween Plugin
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Aqib Gatoo
+Author URI: http://aqibgatoo.com
+License: GPLV2
+*/
