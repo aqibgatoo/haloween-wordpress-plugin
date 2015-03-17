9c7d4fdd8907f1bcdac1f4667ebee6b4261d8c83
Aqib Gatoo <aqib.droid@gmail.com>
2015-03-17 11:00:12 +0530

    Add gitignore file and check typos

--- halloween-plugin.php
+++ halloween-plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Haloween Plugin
+Plugin Name: Halloween Plugin
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
@@ -11,13 +11,23 @@ License: GPLV2
 */
 
 //call when plugin is activated
-register_activation_hook( __FILE__, 'haloween_plugin_install' );
+register_activation_hook( __FILE__, 'halloween_plugin_install' );
 
-function haloween_plugin_install() {
+function halloween_plugin_install() {
 
 	//setup default options values
 	$hween_options_arr = array('currency_sign', '$', );
 
 	//save our default option values
-	update_option( 'haloween_options', $hween_options_arr );
+	update_option( 'halloween_options', $hween_options_arr );
+}
+
+//Hook to initialize products
+add_action( 'init', 'halloween_store_init' );
+
+//initialize the halloween store
+function halloween_store_init() {
+
+
+
 }
